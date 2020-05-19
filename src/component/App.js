c7c9f4a9da5fe52cde23eb190f5b774c861d13de
faithgaiciumia <faithgaiciumia078@gmail.com>
2020-05-19 23:04:16 +0300

    display and button setup

---
+++ src/component/App.js
@@ -0,0 +1,11 @@
+import React from'react';
+import Display from "./Display";
+class App extends React.Component {
+    render(){
+        return(
+            <Display value= "Hey" />
+        )
+    }
+}
+
+export default App;
---
+++ src/component/Button.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
+
+class Button extends React.Component {
+    static propTypes = {
+        name : PropTypes = {
+            name:PropTypes.string,
+            orange: PropTypes.bool,
+            wide:PropTypes.bool,
+            clickHandler: PropTypes.func,
+        }
+    };
+
+    handleClick = () => {
+        this.props.clickHandler (this.props.name);
+    };
+
+    render () {
+        const className = [
+            "component-button",
+            this.props.orange ? "orange" : "",
+            this.props.wide ? "wide" : "",
+
+        ];
+
+        return (
+            <div className= {className.join(" ").trim()} >
+                <button pnClick={this.handleClick}>
+                    {this.props.name}
+                </button>
+            </div>
+        );
+    }
+}

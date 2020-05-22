b9cef70ff57668df1345e177409ac0e72561cc30
faithgaiciumia <faithgaiciumia078@gmail.com>
2020-05-22 11:09:17 +0300

    Updates

---
+++ precache-manifest.ede65636683302efef9a2afb4ae1bcef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2d6fb335e42130b154f500318cd855",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "2cc1cf79a576eb784250",
+    "url": "/reactcalculator/static/css/2.2d5861c9.chunk.css"
+  },
+  {
+    "revision": "85438ec72f36b8310008",
+    "url": "/reactcalculator/static/css/main.75632347.chunk.css"
+  },
+  {
+    "revision": "2cc1cf79a576eb784250",
+    "url": "/reactcalculator/static/js/2.30c4e221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcalculator/static/js/2.30c4e221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85438ec72f36b8310008",
+    "url": "/reactcalculator/static/js/main.18156e46.chunk.js"
+  },
+  {
+    "revision": "38b5d9767e4353d8eb85",
+    "url": "/reactcalculator/static/js/runtime-main.2d5f7ebc.js"
+  }
+]);
